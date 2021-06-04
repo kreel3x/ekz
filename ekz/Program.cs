3499a73adbc9ec657296bfc8f9b574e78dbc8972
Boris <borisvoloshin839@gmail.com>
2021-06-04 14:11:47 +0300

    доделал

--- ekz/Program.cs
+++ ekz/Program.cs
@@ -8,11 +8,11 @@ namespace ekz
         static void Main(string[] args)
         {
             Seans_Kino[] a = new Seans_Kino[5];
-            a[0] = new Seans_Kino(100);
-            a[1] = new Seans_Kino(100);
-            a[2] = new Seans_Kino(15);
-            a[3] = new Seans_Kino(15);
-            a[4] = new Seans_Kino(15);
+            a[0] = new Seans_Kino("Синий Зал",100);
+            a[1] = new Seans_Kino("Желтый Зал", 100);
+            a[2] = new Seans_Kino("Зеленый Зал", 15);
+            a[3] = new Seans_Kino("Фиолетовый Зал", 15);
+            a[4] = new Seans_Kino("Красный Зал", 15);
 
 
 
@@ -37,10 +37,12 @@ namespace ekz
         Random a = new Random();
         private int viewers;
         private int sits;
-        static int nomerSeansa;
+        private int nomerSeansa;
+        private string name;
 
-        public Seans_Kino(int sitsAmount)
+        public Seans_Kino(string name, int sitsAmount)
         {
+            this.name = name;
             sits = sitsAmount;
             if (sitsAmount <= 15)
             {
@@ -55,10 +57,10 @@ namespace ekz
 
         public void Na4aloFilma()
         {
-            nomerSeansa += 1;
-            Console.WriteLine("Начало сеанса №" + nomerSeansa + " со " + viewers + " зрителем/ями.");
+
+            Console.WriteLine("Начало сеанса: " + name + " со " + viewers + " зрителем/ями.");
             Thread.Sleep(5000);
-            Console.WriteLine("Конец сеанса №" + nomerSeansa + ". Все на выход!");
+            Console.WriteLine("Конец сеанса " + name + ". Все на выход!");
             viewers = 0;
         }
         
